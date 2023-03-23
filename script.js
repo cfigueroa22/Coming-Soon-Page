695eaec51c329c88321c52c825ecc0dbfecc3f0e
Carlos Figueroa <carfig7186@students.ecpi.edu>
2023-03-22 21:21:38 -0400

    js completed

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const button = document.getElementsByClassName("submit-button");
+let input = document.querySelector("input");
+
+const validate = (emailInput) => {
+  let email = input.value.trim();
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.length == 0) {
+    alert("Empty");
+  } else {
+    alert("Not Empty");
+  }
+
+  if (emailInput.value.match(mailFormat)) {
+    alert("Thank you!");
+  } else {
+    alert("Enter valid email address!");
+  }
+};
